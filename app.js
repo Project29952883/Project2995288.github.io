e883b4b5ae478ffc07d7ca58e3232b78d03a98ae
ChanelBerma108 <71224645+ChanelBerma108@users.noreply.github.com>
2020-11-10 10:28:37 +0200

    Add files via upload
    Logging Error handling and a second route, working 100%

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express'); 
 const app = express() ; 
+const morgan = require('morgan'); //Middleware
+const peopleRoutes = require('./api/routes/people'); 
+const secondRoutes = require('./api/routes/secondroute'); 
+
+app.use(morgan('dev')) ;
+
+
+app.use('/people', peopleRoutes) ;
+app.use('/secondroute', secondRoutes) ;
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works '
-    }) ; 
-}) ;
-
+    const error = new Error('Not found'); 
+    error.status = 404  ; 
+    next(error) ; 
+})
+app.use((error, req, res, next) => {
+res.status(error.status || 500 ); 
+res.json({ 
+    error: {
+        message: error.message 
+    }
+})
+}); 
 module.exports = app ; 
